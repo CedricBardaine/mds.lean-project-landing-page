7b6150da86f28393ae746e4e540967a2bb2c3597
Cédric B <cedricbardaine@gmail.com>
2021-02-10 15:16:32 +0100

    try some Locomotive code but no effect

--- src/App.vue
+++ src/App.vue
@@ -49,11 +49,19 @@
           type="list-item-avatar-three-line, image, article, image, article, image, article"
         ></v-skeleton-loader>
       </v-container>
+
+      <div class="red" style="height: 12px"></div>
+
+    <img width="25%" style="height: 200px" src="./assets/logo.png">
+
+<Helloscroll/>
+
+      <div class="red" style="height: 12px"></div>
+
       <div>
         var de dev lisez pas :
         <span class="red white--text px-2">
-        ({{$vuetify.breakpoint.name}})
-
+          ({{ $vuetify.breakpoint.name }})
         </span>
       </div>
     </v-main>
@@ -61,13 +69,107 @@
 </template>
 
 <script>
+// import LocomotiveScroll from "locomotive-scroll";
+import "./assets/css/locomotive-scroll.css";
+
+import Helloscroll from "./components/Helloscroll";
+
 export default {
   name: "App",
 
-  components: {},
+  components: {Helloscroll,},
 
   data: () => ({
     //
   }),
+  mounted() {
+  },
 };
 </script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Quicksand", sans-serif;
+  color: #010101;
+}
+h4,
+h1 {
+  text-align: center;
+}
+h4 {
+  font-weight: 600;
+  font-size: 2em;
+  margin-bottom: 0;
+}
+h1 {
+  font-weight: 300;
+  font-size: 4em;
+  margin: 0;
+}
+.container {
+  display: flex;
+  width: 100%;
+  height: 58vh;
+  margin-top: 19em;
+}
+.getting-started {
+  background-color: #e8e5e6;
+  width: 30%;
+  padding: 3em;
+}
+.getting-started h4 {
+  text-align: left;
+}
+.getting-started p {
+  font-size: 1.5em;
+}
+button {
+  padding: 1.4em;
+  width: 40%;
+  background-color: #010101;
+  color: #ffffff;
+  border: none;
+  outline: none;
+}
+
+.image-holder {
+  width: 70%;
+}
+img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: 50% 50%;
+}
+.gallery {
+  padding: 2em;
+  height: 200vh;
+}
+.image-pop {
+  width: 20%;
+  height: 30vh;
+  position: relative;
+}
+#one {
+  top: 3em;
+  left: 5em;
+}
+#two {
+  top: 4em;
+  left: 60%;
+}
+#three {
+  top: 8em;
+  left: 10%;
+}
+#four {
+  top: 3em;
+  left: 70%;
+}
+#five {
+  top: 6em;
+  left: 30%;
+}
+</style>
