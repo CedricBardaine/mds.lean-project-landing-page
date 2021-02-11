4f7b66922c08bfc70120c02be93582398780966d
Cédric B <cedricbardaine@gmail.com>
2021-02-11 14:40:56 +0100

    add wait at the end of the deploy script

--- deploy.sh
+++ deploy.sh
@@ -21,3 +21,17 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:CedricBardaine/mds.lean-project-landing-page.git master:gh-pages
+
+
+
+
+
+cd -
+
+
+# PAUSE
+# cmd /k
+read varpause
+
+
+# <USERNAME> : CedricBardaine
