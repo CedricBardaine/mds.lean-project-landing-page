13b0e7c50a18041b6fc5b528237bbf440e7fd9fa
Cédric B <cedricbardaine@gmail.com>
2021-02-11 17:02:57 +0100

    make icons bigger

--- src/components/CardAvantage.vue
+++ src/components/CardAvantage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card-advantage">
     <div class="icon-container">
-      <v-icon class="icon"> {{ icon }} </v-icon>
+      <v-icon class="icon" style="font-size: 32px"> {{ icon }} </v-icon>
     </div>
     <h3 class="c-title">{{ title }}</h3>
     <div class="c-content">{{ content }}</div>
